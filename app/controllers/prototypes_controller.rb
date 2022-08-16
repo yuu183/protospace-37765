f10c447d4a619bd335ceb1c6e3d0cbf57dc6b397
yuu183 <y.toyama888@gmail.com>
2022-08-16 15:18:43 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :prototype_set , except: [:index, :new, :create]
   before_action :authenticate_user! ,except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  
   def index
     @prototype = Prototype.includes(:user)
   end
@@ -19,17 +21,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -38,7 +37,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -47,13 +45,16 @@ class PrototypesController < ApplicationController
   end
 
   private
+
+  def prototype_set 
+    @prototype = Prototype.find(params[:id])
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to root_path 
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
